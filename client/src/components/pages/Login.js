35c6b0dcf6804d6305403ebc15e1b1e1fdce86a2
A <andcooke@gmail.com>
2022-08-31 13:29:31 -0500

    created a few source files

---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { GoogleLogin } from 'react-google-login';
+
+export default function Login () {
+
+
+  return (
+    <div>
+      <form>
+        <div className="login-input-container">
+          <input name="username" className="login-username" placeholder="username"></input>
+          <input name="password" className="login-password" placeholder="password"></input>
+        </div>
+        <div>
+          <button className="submit-login">submit</button>
+        </div>
+      </form>
+    </div>
+  )
+};
