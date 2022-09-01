a08c7e49e3b8a8987ad5bb1f43d08198f09d30b2
A <andcooke@gmail.com>
2022-09-01 10:23:14 -0500

    created more public folders and files

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { GoogleLogin } from 'react-google-login';
+// import { GoogleLogin } from 'react-google-login'????????;
 
 export default function Login () {
 
