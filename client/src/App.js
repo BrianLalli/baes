ae6392d7c0372e74afb0fa7f684c393450e8132c
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-09-02 12:09:38 -0500

    added navbar and footer with functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,76 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import GetStarted from './pages/GetStarted';
-import Login from './pages/Login';
-import Home from './pages/Home/Home';
+import React, { useState } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Style from './MainLayout.module.scss'
+import GetStarted from "./pages/GetStarted";
+import Login from "./pages/Login";
+import Home from "./pages/Home/Home";
 // import Profile from './pages/Profile';
-// import Header from './components/Header';
+import Navbar from "./components/Navbar/index";
+import Toggler from "./components/Toggler/Toggler";
+import Style from "./App.module.scss";
+import { Box, Grid } from "@mui/material";
 // import Footer from './components/Footer';
+// import {Box, Grid} from "@mui/material";
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const handleClick = () => setDarkMode(!darkMode);
   return (
-    <ApolloProvider client={client}>
-      {/* Wrap page elements in Router component to keep track of location state */}
-      <Router>
-        {/* <div className="flex-column justify-flex-start min-100-vh"> */}
+    <Box className={darkMode ? Style.dark : Style.light}>
+      <ApolloProvider client={client}>
+        {/* Wrap page elements in Router component to keep track of location state */}
+        <Router>
+          <Grid
+            container
+            display={"flex"}
+            flexDirection={"column"}
+            minHeight={"100vh"}
+            justifyContent={"space-between"}
+          ></Grid>
+          <Grid item>
+            <Navbar darkMode={darkMode} handleClick={handleClick} />
+          </Grid>{" "}
+          {/* <div className="flex-column justify-flex-start min-100-vh"> */}
           {/* <Header />
           <div className="container"> */}
-            {/* Wrap Route elements in a Routes component */}
-            <Routes>
-              {/* Define routes using the Route component to render different page components at different paths */}
-              {/* Define a default route that will render the Home component */}
-              <Route 
-                path="/" 
-                element={<GetStarted />} 
-              />
-              {/* Define a route that will take in variable data */}
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              {/* Define a route that will take in variable data */}
-              <Route 
-                path="/home" 
-                element={<Home />} 
-              />
-            </Routes>
+          {/* Wrap Route elements in a Routes component */}
+          <Routes>
+            {/* Define routes using the Route component to render different page components at different paths */}
+            {/* Define a default route that will render the Home component */}
+            <Route path="/" element={<GetStarted />} />
+            {/* Define a route that will take in variable data */}
+            <Route path="/login" element={<Login />} />
+            {/* Define a route that will take in variable data */}
+            <Route path="/home" element={<Home />} />
+          </Routes>
           {/* </div>
           {/* <Footer /> */}
-        {/* </div> */} 
-      </Router>
-    </ApolloProvider>
+          <Grid item>
+            <Box
+              component={"footer"}
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+              py={"1.5rem"}
+              sx={{ opacity: 0.7 }}
+              width={"100%"}
+            >
+              <p>
+                Lovers Not Fighters &copy; 2022
+                <a href={"https://github.com/BrianLalli/baes"}></a>
+              </p>
+            </Box>
+          </Grid>
+          {/* </div> */}
+        </Router>
+      </ApolloProvider>
+    </Box>
   );
 }
 
